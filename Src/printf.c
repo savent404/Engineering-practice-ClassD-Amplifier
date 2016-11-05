e74bd07c47a74f560ea93e815f6e3b1d187b23d7
廖元垲 <Savent_gate@outlook.com>
2016-11-05 16:29:52 +0800

    basic cmd is donw

--- Src/printf.c
+++ Src/printf.c
@@ -1,16 +1,35 @@
 #include <stm32f1xx_hal.h>
+#include <string.h>
 extern UART_HandleTypeDef huart1;
-
+static uint8_t tx_buf[30];
+static __IO uint8_t rx_buf[30];
+static __IO uint8_t rx_cnt = 0;
+static __IO uint8_t rx_flag = 0;
 int stdout_putchar (int ch) {
-	HAL_UART_Transmit(&huart1, (uint8_t*)&ch, 1, 10);
+	static uint8_t cnt = 0;
+	if (ch != '\n')
+		tx_buf[cnt++] = ch;
+	else {
+		tx_buf[cnt++] = ch;
+		HAL_UART_Transmit_IT(&huart1, (uint8_t*)&tx_buf, cnt);
+		cnt = 0;
+	}
 	return ch;
 }
 int stderr_putchar (int ch) {
-	while (HAL_UART_Transmit(&huart1, (uint8_t*)&ch, 1, 10) != HAL_OK);
+	HAL_UART_Transmit_IT(&huart1, (uint8_t*)&ch, 1);
 	return ch;
+}
+int usr_puts(char *pt) {
+	
+}
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	rx_flag = 1;
 }
 int stdin_getchar(void) {
 	uint8_t buf[1];
-	while (HAL_UART_Receive(&huart1, buf, 1, 10) != HAL_OK);
-	return *buf;
+	HAL_UART_Receive_IT(&huart1, (uint8_t*)(rx_buf + rx_cnt), 1);
+	while (!rx_flag);
+	rx_flag = 0;
+	return *(rx_buf + rx_cnt);
 }
--- Src/stm32f1xx_hal_msp.c
+++ Src/stm32f1xx_hal_msp.c
@@ -163,6 +163,20 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
 
   /* USER CODE END TIM1_MspInit 1 */
   }
+  else if(htim_base->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspInit 0 */
+
+  /* USER CODE END TIM3_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM3_CLK_ENABLE();
+    /* Peripheral interrupt init */
+    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(TIM3_IRQn);
+  /* USER CODE BEGIN TIM3_MspInit 1 */
+
+  /* USER CODE END TIM3_MspInit 1 */
+  }
 
 }
 
@@ -201,10 +215,25 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
   /* USER CODE END TIM1_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_TIM1_CLK_DISABLE();
-  }
   /* USER CODE BEGIN TIM1_MspDeInit 1 */
 
   /* USER CODE END TIM1_MspDeInit 1 */
+  }
+  else if(htim_base->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspDeInit 0 */
+
+  /* USER CODE END TIM3_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM3_CLK_DISABLE();
+
+    /* Peripheral interrupt DeInit*/
+    HAL_NVIC_DisableIRQ(TIM3_IRQn);
+
+  /* USER CODE BEGIN TIM3_MspDeInit 1 */
+
+  /* USER CODE END TIM3_MspDeInit 1 */
+  }
 
 }
 
@@ -234,6 +263,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+    /* Peripheral interrupt init */
+    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART1_IRQn);
   /* USER CODE BEGIN USART1_MspInit 1 */
 
   /* USER CODE END USART1_MspInit 1 */
@@ -257,6 +289,9 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     PA10     ------> USART1_RX 
     */
     HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9|GPIO_PIN_10);
+
+    /* Peripheral interrupt DeInit*/
+    HAL_NVIC_DisableIRQ(USART1_IRQn);
 
   }
   /* USER CODE BEGIN USART1_MspDeInit 1 */
